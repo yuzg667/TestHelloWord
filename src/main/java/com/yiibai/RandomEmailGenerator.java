6476efa1d7e92d74ed7c56200a774313ade8bd41
yuzg <yuzg@yuzgdeMacBook-Pro.local>
2020-12-09 10:17:29 +0800

    1209-001

---
+++ src/main/java/com/yiibai/RandomEmailGenerator.java
@@ -0,0 +1,11 @@
+package com.yiibai;
+
+//import org.springframework.stereotype.Service;
+
+public class RandomEmailGenerator {
+
+    public String generate() {
+        return "feedback@yiibai.com";
+    }
+
+}
