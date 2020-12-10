4f89434b1900dccf9b20e1c080a7f06f0c9d5b20
yuzg <yuzg@yuzgdeMacBook-Pro.local>
2020-12-10 18:03:00 +0800

    增加空用例4

--- src/main/java/com/yiibai10/TestEmpty3.java
+++ src/main/java/com/yiibai10/TestEmpty3.java
@@ -1,16 +1,16 @@
-//package com.yiibai10;
-//
-//import org.testng.Assert;
-//import org.testng.ITestContext;
-//import org.testng.annotations.DataProvider;
-//import org.testng.annotations.Test;
-//
-//@Test
-//public class TestEmpty3 {
-//	
-//	@Test
-//    public void test1(String number) {
-//    }
-//
-//}
-//	
+package com.yiibai10;
+
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@Test
+public class TestEmpty3 {
+	
+	@Test
+    public void test1(String number) {
+    }
+
+}
+	
