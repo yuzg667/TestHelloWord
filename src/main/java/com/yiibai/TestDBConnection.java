7488d6737176705a1896f92f9b75cfbef998f8c0
yuzg <yuzg@yuzgdeMacBook-Pro.local>
2020-12-18 12:05:44 +0800

    恢复一个文件

---
+++ src/main/java/com/yiibai/TestDBConnection.java
@@ -0,0 +1,17 @@
+package com.yiibai;
+
+import org.testng.annotations.Test;
+
+public class TestDBConnection {
+
+    @Test
+    public void runOtherTest1() {
+        System.out.println("@Test - runOtherTest1");
+    }
+
+    @Test
+    public void runOtherTest2() {
+        System.out.println("@Test - runOtherTest2");
+    }
+
+}
