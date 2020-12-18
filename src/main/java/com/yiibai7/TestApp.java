a9144d4db9eae9b31f5b32560c4be37247b56e42
yuzg <yuzg@yuzgdeMacBook-Pro.local>
2020-12-18 11:29:32 +0800

    src/main/java/com/yiibai7/TestApp.java

--- src/main/java/com/yiibai7/TestApp.java
+++ src/main/java/com/yiibai7/TestApp.java
@@ -7,14 +7,14 @@ public class TestApp {
     //Run if all methods from "deploy" and "db" groups are passed.
     @Test(dependsOnGroups={"deploy","db"})
     public void method1() {
-        System.out.println("This is method 1");
+        System.out.println("This is method 1TestApp");
         //throw new RuntimeException();
     }
 
     //Run if method1() is passed.
     @Test(dependsOnMethods = { "method1" })
     public void method2() {
-        System.out.println("This is method 2");
+        System.out.println("This is method 2TestApp");
     }
 
 }//原文出自【易百教程】，商业转载请联系作者获得授权，非商业请保留原文链接：https://www.yiibai.com/testng/dependency_test.html
